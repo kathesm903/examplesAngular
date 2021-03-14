a37e639f33eaa40cde50db4341f0320a81012da6
kathesm903 <kathesm903@gmail.com>
2021-03-14 00:24:40 -0500

    Create main component and suma component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { SumaComponent } from './suma/suma.component';
+import { MainComponent } from './main/main.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SumaComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule
